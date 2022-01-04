0a43d1fd4c8fac47ce6ab3e4c0922f76d78f0204
Olga Shilenkova <Olga1shilenkova@gmail.com>
2022-01-03 22:29:38 -0600

    gueries with group by, having, order by

---
+++ 3_queries_group-by/large_cohorts.sql
@@ -0,0 +1,8 @@
+SELECT cohorts.name , count(students.*) as total_students
+FROM students 
+JOIN cohorts 
+ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(students.*)>=18
+ORDER BY total_students
+;
