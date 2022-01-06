3572654a763c4820927c88d73dd9d9e772d3dc32
Olga Shilenkova <Olga1shilenkova@gmail.com>
2022-01-06 12:10:29 -0600

    Update to use parameterized queries.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,14 +8,26 @@ const pool = new Pool({
   port: 5432
 });
 
-pool.
-query(`
-  SELECT students.id as id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3]||5};
-  `)
+// 
+// separate out our SQL
+// 
+//The part that we write as the developer, the part that we have complete control over.
+const queryString =`
+SELECT students.id as id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+// The part that comes from somewhere else and might be malicious.
+const cohortName = process.argv[2];
+const limit = process.argv[3]|| 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool
+.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,24 @@ const pool = new Pool({
   port: 5432
 });
 
-pool.
-query(`
+
+const queryString = `
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+  WHERE cohorts.name  LIKE $1 
   ORDER BY teacher;
-  `)
+  `;
+  // Query values store in array that starts from 1, not from 0 that is where $1 comes from
+
+  const cohortName = process.argv[2] || 'JUL02';//$1 in queryString
+  
+  const values = [`%${cohortName}%`]
+
+pool.
+query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher} \n`);
